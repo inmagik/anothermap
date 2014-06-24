54d29b0d0680ee8152774e9f8aed8544feead3dc
Mauro Bianchi <bianchimro@gmail.com>
2014-06-24 12:28:07 +0200

    code cleanup

--- js/services/wms-query-service.js
+++ js/services/wms-query-service.js
@@ -4,22 +4,15 @@
     angular.module('pocketMap')
     .factory('wmsQueryService', [ '$q', 'layersManager', function($q, layersManager){
 
-        var svc = {
-
-            
-            
-        };
+        var svc = {};
 
         var formatData = function(d){
-
             var out = "";
             for(var x in d){
                 var a = d[x].response;
                 var s = a.split("\n")                
-                console.log("s", s)
                 s = _.reject(s, function(item){return (item == 'GetFeatureInfo results:' || item.replace(" ", "") == "") })
                 var j = "<h4>"+d[x].name+"</h4>" + s.join("<br>");
-
                 out = out + j;
             }
             return out;
@@ -27,14 +20,12 @@
 
         svc.queryPoint = function(point, mapId, view){
             var out = $q.defer();
-            console.log("queryPoint", point)
             var layers = _.reject(
                 layersManager.layersForMaps[mapId] || [],
                 function(item){
                     return item.wmsQueryable !== true || item.layer.getVisible() != true;
                 }
             );
-            
             var que = [];
             var data = {};
 
@@ -48,35 +39,23 @@
                     view.getProjection(),
                     {}
                 );
-                console.log("url", url);
                 que.push(url);
-
-                $.get(url).success(function(response){
+                $.get(url).done(function(response){
+                    data[url] = { response : response, name : n};
+                }).always(function(){
                     var p = que.indexOf(url);
                     que.splice(p, 1);
-                    data[url] = { response : response, name : n};
-
                     if(que.length==0){
                         out.resolve({data:data, formatted:formatData(data)});
                     }
-
-                })
-
+                });
             });
-
-
             return out.promise;
 
-
-        }
-
-
-
-    
-
+        };
         return svc;
-    }]);
     
+    }]);
 
 }());
 
